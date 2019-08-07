70ee2742803a328852bc426119f0e4fae59daa4e
Rebecca Doebler <rdoebler@umich.edu>
2019-08-07 18:49:10 -0400

    Modifications to files for cleanup and refactoring

--- dynamixel_control.cpp
+++ dynamixel_control.cpp
@@ -1,81 +1,56 @@
-
-// This header defines the standard ROS classes.
+/*****************************************************
+ *              Header Includes                      *
+ *****************************************************/
 #include <ros/ros.h>
 #include "dynamixel_includes.h"
 #include "dynamixel_control/ServoPosition.h"
 
+/*****************************************************
+ *              Prototypes                           *
+ *****************************************************/
+static void Ros_Init(int argc, char **argv); 
+static bool Dynamixel_Init(void); 
+
+/*****************************************************
+ *             File Scope Variables                  *
+ *****************************************************/
 static uint8_t left_right_id; 
 static uint8_t forward_back_id;
 static DynamixelWorkbench my_dynamixel; 
-static bool increment = true; 
+static dynamixel_control::ServoPosition myServoPosition;
+static ros:: Publisher pub; 
 
 /*****************************************************
- *              Periodic Timer Callback              *
- *              for getting current position of      *
- *              Dynamixel.                           *
+ *              Initializations for ROS              *
  *****************************************************/
-void readCurPositionTimer(const ros::TimerEvent& event)
+static void Ros_Init(int argc, char **argv)
 {
-  const char* log;  
-  int32_t     current_position; 
 
-  /* Local init */
-  current_position = 0U; 
-
-  /* Read and print the current position */
-  my_dynamixel.getPresentPositionData(left_right_id, &current_position, &log); 
-
-  if(increment == true)
-  {
-    ROS_INFO("Incrementing");     
-    current_position = current_position + 10;
-    
-    if(current_position >= 350)
-    {
-      increment = false;  
-    }
-  }
-  else
-  {
-    ROS_INFO("Decrementing");     
-    current_position = current_position - 10; 
-    
-    if(current_position <= 50)
-    {
-      increment = true; 
-    }
-  }
-
-  my_dynamixel.goalPosition(left_right_id, current_position); 
-
-  ROS_INFO("Current position: %d", current_position); 
-}
-
-/*****************************************************
- *              Main Entry Point                     *
- *****************************************************/
-int main(int argc, char **argv) 
-{
-  bool               result;
-  uint32_t           baud_rate;
-  const char*        log;
-  const char*        device_name; 
-  
-  /* Local Inits */
-  result          = false;
-  baud_rate       = 1000000;
-  device_name     = "/dev/ttyUSB0";
-  left_right_id   = 2U;
-  forward_back_id = 1U;  
-  
   /* Initialize the ROS system. */
   ros::init(argc, argv, "dynamixel_control_rebecca");
 
   /* Establish this program as a ROS node. */
   ros::NodeHandle nh;
 
-  /* Send some output as a log message. */
-  ROS_INFO_STREAM("Starting Dynamixel Node!");
+  /* Set up the servo publisher and subscriber */
+  pub = nh.advertise<dynamixel_control::ServoPosition>("dynamixel_control_rebecca", 100); 
+}
+
+/*****************************************************
+ *              Initializations for the              *
+                Dynamixels                           *
+ *****************************************************/
+static bool Dynamixel_Init(void)
+{
+  bool        result;
+  uint32_t    baud_rate;
+  const char* log;
+  const char* device_name; 
+
+  /* Local inits */ 
+  result      = false;  
+  baud_rate   = 1000000;
+  device_name = "/dev/ttyUSB0";
 
   /* Initialize the necessary USB port for dynamixel communication */
   if(my_dynamixel.init(device_name, baud_rate, &log) == true) 
@@ -83,23 +58,53 @@ int main(int argc, char **argv)
     /* Ping Dynamixel to see if it is on */
     if((my_dynamixel.ping(left_right_id, &log) == true) && (my_dynamixel.ping(forward_back_id, &log)))
     {
+      /* Send some output as a log message. */
+      ROS_INFO_STREAM("Starting Dynamixel Node!"); 
+     
       /* Turn on the torque */       
       my_dynamixel.torqueOn(left_right_id, &log); 
-      my_dynamixel.torqueOn(forward_back_id, &log);     
+      my_dynamixel.torqueOn(forward_back_id, &log); 
       
-      /* Start a timer to read the current position every 10ms */
-      ros::Timer timer = nh.createTimer(ros::Duration(0.1), readCurPositionTimer);
+      result = true; 
+    }
+  }
 
-      /* Loop forever */
-      ros::spin();
-    }
-    else
-    {
-      ROS_ERROR("Couldn't ping Dynamixels. Double check that your system is powered on!"); 
-    }
+  return(result); 
+}
+
+/*****************************************************
+ *              Main Entry Point                     *
+ *****************************************************/
+int main(int argc, char **argv) 
+{  
+  const char* log;  
+  
+  /* Local Inits */
+  left_right_id   = 2U;
+  forward_back_id = 1U;  
+  
+  /* Initialize ROS nodes and variables */  
+  Ros_Init(argc, argv);
+
+  /* Set up the rate */
+  ros:: Rate rate(100); 
+  
+  /* Initialize the necessary USB port for dynamixel communication */
+  if(Dynamixel_Init() == true)     
+  {
+    /* Loop forever */    
+    while(ros::ok())
+    { 
+      /* Get the current position of the servo and publish it */
+      my_dynamixel.getPresentPositionData(left_right_id, &myServoPosition.servoCurrentPosition, &log);
+      pub.publish(myServoPosition); 
+  
+      ROS_INFO("Current position: %d", myServoPosition.servoCurrentPosition);
+      rate.sleep();
+     }      
   }
   else
   {
-    ROS_ERROR("Couldn't open USB port! Double check that your USB connector is plugged in!"); 
+    ROS_ERROR("Couldn't connect to servo! Check that the USB is plugged in and the servo is powered on!"); 
   }
 }
--- dynamixel_includes.h
+++ dynamixel_includes.h
@@ -3,12 +3,6 @@
 #ifndef DYNAMIXEL_INCLUDES_H_
 #define DYNAMIXEL_INCLUDES_H_
 
-//#define SIMULATE_DYNAMIXEL
-
-#ifdef SIMULATE_DYNAMIXEL
-	#include "stub/dynamixel_stub.h"
-#else
-	#include "/home/rebecca/projects/ECE591_WS/src/dynamixel-workbench/dynamixel_workbench_toolbox/include/dynamixel_workbench_toolbox/dynamixel_workbench.h"
-#endif
+#include "/home/rebecca/projects/ECE591_WS/src/dynamixel-workbench/dynamixel_workbench_toolbox/include/dynamixel_workbench_toolbox/dynamixel_workbench.h"
 
 #endif /*DYNAMIXEL_INCLUDES_H_*/
