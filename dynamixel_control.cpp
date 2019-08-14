c8c9fd111ade00309b35601d3c55a41934d92dc8
Rebecca Doebler <rdoebler@umich.edu>
2019-08-14 19:40:07 -0400

    Updated to use degrees and radians

--- dynamixel_control.cpp
+++ dynamixel_control.cpp
@@ -17,8 +17,64 @@ static bool Dynamixel_Init(void);
 static uint8_t left_right_id; 
 static uint8_t forward_back_id;
 static DynamixelWorkbench my_dynamixel; 
-static dynamixel_control::ServoPosition myServoPosition;
-static ros:: Publisher pub; 
+static ros:: Subscriber sub;
+
+/*****************************************************
+ *            Definitions                            *
+ *****************************************************/
+#define SERVO_DEGREE_MIN    -130.0f
+#define SERVO_DEGREE_MAX    -50.0f
+#define SERVO_DEGREE_CENTER -90.0f
+#define RAD2DEG(x) x * (180.0/3.141592653589793238463)
+#define DEG2RAD(x) x * (3.141592653589793238463/180.0)
+
+/*****************************************************
+ *              Callback for accepting               *
+ *              the servo position goal.             *
+ *****************************************************/
+void goalPositionCallback(dynamixel_control::ServoPosition requestedServoPosition)
+{
+  float       currentDegreeValue; 
+  float       requestedDegreeValue; 
+  float       servoRadValue; 
+  float       radianValue;
+  const char* log;
+ 
+  my_dynamixel.getRadian(left_right_id, &radianValue, &log); 
+  currentDegreeValue = RAD2DEG(radianValue);
+  
+  /* Convert degrees to radians */
+  if(requestedServoPosition.servoClockWiseRotation == 0)
+  {
+    requestedDegreeValue = currentDegreeValue + requestedServoPosition.servoDegreeRotation; 
+  }
+  else
+  {
+    requestedDegreeValue = currentDegreeValue - requestedServoPosition.servoDegreeRotation; 
+  }
+
+  /* Bounds check */
+  if(requestedDegreeValue < SERVO_DEGREE_MIN)
+  {
+     requestedDegreeValue = SERVO_DEGREE_MIN; 
+  }
+  else if(requestedDegreeValue > SERVO_DEGREE_MAX)
+  {
+     requestedDegreeValue = SERVO_DEGREE_MAX; 
+  }
+  else
+  {
+     /* Do nothing */
+  }
+
+  ROS_INFO("Requested final degree: %f", requestedDegreeValue);
+  
+  /* Convert back to radians */
+  servoRadValue = DEG2RAD(requestedDegreeValue); 
+ 
+  /* Send the position to the servo */
+  my_dynamixel.goalPosition(left_right_id, servoRadValue, &log);
+}
 
 /*****************************************************
  *              Initializations for ROS              *
@@ -32,8 +88,8 @@ static void Ros_Init(int argc, char **argv)
   /* Establish this program as a ROS node. */
   ros::NodeHandle nh;
 
-  /* Set up the servo publisher and subscriber */
-  pub = nh.advertise<dynamixel_control::ServoPosition>("dynamixel_control_rebecca", 100); 
+  /* Set up the servo subscriber */
+  sub = nh.subscribe<dynamixel_control::ServoPosition>("dynamixel_control_rebecca", 100, goalPositionCallback);
 }
 
 /*****************************************************
@@ -77,7 +133,9 @@ static bool Dynamixel_Init(void)
  *****************************************************/
 int main(int argc, char **argv) 
 {  
-  const char* log;  
+  const char* log;
+  float       radianValue; 
+  int32_t     servoCurrentPosition;    
   
   /* Local Inits */
   left_right_id   = 2U;
@@ -87,19 +145,22 @@ int main(int argc, char **argv)
   Ros_Init(argc, argv);
 
   /* Set up the rate */
-  ros:: Rate rate(100); 
+  ros:: Rate rate(10); 
   
   /* Initialize the necessary USB port for dynamixel communication */
   if(Dynamixel_Init() == true)     
   {
+    /* Start up servo centered */
+    radianValue = DEG2RAD(SERVO_DEGREE_CENTER); 
+    my_dynamixel.goalPosition(left_right_id, radianValue, &log);
+
     /* Loop forever */    
     while(ros::ok())
-    { 
-      /* Get the current position of the servo and publish it */
-      my_dynamixel.getPresentPositionData(left_right_id, &myServoPosition.servoCurrentPosition, &log);
-      pub.publish(myServoPosition); 
-  
-      ROS_INFO("Current position: %d", myServoPosition.servoCurrentPosition);
+    {    
+      /* Trigger callback to fire and grab latest data and update the servo */
+      ros::spinOnce(); 
+   
+      /* Wait for some time */
       rate.sleep();
      }      
   }
